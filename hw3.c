65b4a588c3cc80c4be5c8cadda7c001f07939b42
Peter <peter.cieslik@gmail.com>
2018-10-15 19:27:02 -0500

    fixed char arr's

--- hw3.c
+++ hw3.c
@@ -12,28 +12,36 @@
 
 void sigint_handler(int sig)
 {
-    char msg[] = "Signal handled. \nCS361 >";
+    char msg[] = "caught sigint\nCS361 >";
     write(1, msg, sizeof(msg));
 }
 void sigtstp_handler(int sig)
 {
-    char msg[] = "Signal handled. \nCS361 >";
+    char msg[] = "caught sigstp\nCS361 >";
     write(1, msg, sizeof(msg));
 }
 int main()
 {
-    int count, pos, status, stdi = dup(0), stdo = dup(1), runs = 1;
+    int count, pos, status = 0, stdi = dup(0), stdo = dup(1), runs = 1;
     char *arg, *filename = NULL, *redir = NULL, *arr = (char *)malloc(arrSz * sizeof(char)),
                **args = malloc(arrSz * sizeof(char *)), **args2 = malloc(arrSz * sizeof(char *));
-    
+
     signal(SIGINT, sigint_handler);
     signal(SIGTSTP, sigtstp_handler);
     do
     {
         printf("CS361 > ");
+        arr[0]=0;
+        for (int i = 0; i < arrSz; i++)
+        {
+            args[i] = NULL;
+            args2[i] = NULL;
+        }
         arg = strtok(fgets(arr, arrSz, stdin), " \n");
-        if (strcmp(arg, "exit")==0)
+        if (strcmp(arg, "exit") == 0)
             return 0;
+        dup2(stdo, 1);
+        dup2(stdi, 0);
         count = 0;
         while (arg != NULL)
         {
@@ -42,6 +50,7 @@ int main()
                 redir = arg;
                 arg = strtok(NULL, " \n");
                 filename = arg;
+                puts(filename);
                 arg = NULL;
             }
             else if (strcmp(arg, ";") == 0)
@@ -65,13 +74,11 @@ int main()
                 arg = strtok(NULL, " \n");
             }
         }
-
         pid_t pid = fork();
 
         if (pid > 0)
         {
-            //wait(NULL);
-            waitpid(pid, &status, 0);
+            wait(NULL);
             if (strcmp(redir, ";") == 0)
             {
                 pid_t pid2 = fork();
@@ -79,8 +86,7 @@ int main()
                     execvp(args2[0], args2);
                 else if (pid2 > 0)
                 {
-                    waitpid(pid, &status, 0);
-                    //wait(NULL);
+                    wait(NULL);
                     printf("pid:%d status:%d\n", pid2, status);
                 }
 
@@ -102,14 +108,14 @@ int main()
         {
             if (strcmp(redir, ">") == 0)
             {
-                //dup2(stdo, 1);
                 dup2(open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IWOTH), 1);
+                execvp(args[0], args);
             }
 
             else if (strcmp(redir, "<") == 0)
             {
-                //dup2(stdi, 0);
                 dup2(open(filename, O_RDONLY, S_IROTH), 0);
+                execvp(args[0], args);
             }
             execvp(args[0], args);
         }
